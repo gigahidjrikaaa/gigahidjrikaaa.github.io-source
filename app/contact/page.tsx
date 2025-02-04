1d7b63509acc927c75e6f81da630a671fed81f70
Giga Hidjrika Aura Adkhy <52191436+gigahidjrikaaa@users.noreply.github.com>
2025-02-05 00:20:19 +0700

    feat: Add image to Contact Page and improve layout

--- app/contact/page.tsx
+++ app/contact/page.tsx
@@ -1,11 +1,14 @@
 import React, { FC } from 'react';
 import { Button } from '@/components/ui/button';
+import Image from 'next/image';
 
 const ContactPage: FC = () => {
     return (
-        <div className='flex flex-col justify-center items-center'>
-            <h1 className='font-bold text-3xl'>Contact Page</h1>
-            {/* Add your contact form or any other content here */}
+        <div className='flex flex-col h-screen min-h-fit'>
+            <div className='w-full h-1/2 relative'>
+                <Image src='/images/giga-dock.jpg' objectFit='cover' layout='fill' alt='Contact' />
+                <h1 className='absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 text-white text-4xl font-bold'>Contact Me!</h1>
+            </div>
             <Button>Submit</Button>
         </div>
     );
