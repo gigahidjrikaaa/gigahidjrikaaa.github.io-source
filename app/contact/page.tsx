cb69d86c1ba74b1dd703fb18b4c817527b4b0eba
Giga Hidjrika Aura Adkhy <52191436+gigahidjrikaaa@users.noreply.github.com>
2025-02-04 23:59:45 +0700

    chore: Update project dependencies and configuration files

---
+++ app/contact/page.tsx
@@ -0,0 +1,14 @@
+import React, { FC } from 'react';
+import { Button } from '@/components/ui/button';
+
+const ContactPage: FC = () => {
+    return (
+        <div>
+            <h1>Contact Page</h1>
+            {/* Add your contact form or any other content here */}
+            <Button>Submit</Button>
+        </div>
+    );
+};
+
+export default ContactPage;
